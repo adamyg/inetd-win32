32428717d6d8518b2e5a6119fe404524aa53f70f
adamyg <adamyg@gmail.com>
2022-05-09 13:57:21 +0800

    quiet mode

--- mmdblookup/lookup.cpp
+++ mmdblookup/lookup.cpp
@@ -25,27 +25,27 @@
 #pragma comment(lib, "ws2_32.lib")
 
 #include <maxminddb/maxminddb.h>
-
 #endif
 
 #include "../libinetd/ServiceGetOpt.h"
 
 #if defined(HAVE_LIBMAXMINDDB)
-static bool         mmdbopen(const char *filename, MMDB_s *mmdb);
-static void         mmdbmeta(MMDB_s *mmdb);
-static int          mmdblookup(MMDB_s *mmdb, const char *ip_address);
+static bool	mmdbopen(const char *filename, MMDB_s *mmdb);
+static void	mmdbmeta(MMDB_s *mmdb);
+static int	mmdblookup(MMDB_s *mmdb, const char *ip_address, bool quiet);
 #endif
-static void         usage(const inetd::Getopt &options, const char *fmt, ...); /*no-return*/
+static void	usage(const inetd::Getopt &options, const char *fmt, ...); /*no-return*/
 
-static const char *short_options = "f:i:v";
+static const char *short_options = "f:i:q:v";
 static struct inetd::Getopt::Option long_options[] = {
-    { "db",         inetd::Getopt::argument_required,   NULL,   'd'  },
-    { "ip",         inetd::Getopt::argument_required,   NULL,   'i'  },
-    { "file",       inetd::Getopt::argument_required,   NULL,   'f'  },
-    { "verbose",    inetd::Getopt::argument_none,       NULL,   'v'  },
-    { "usage",      inetd::Getopt::argument_none,       NULL,   1100 },
-    { NULL }
-    };
+	{ "db", 	inetd::Getopt::argument_required,   NULL,   'd'  },
+	{ "ip", 	inetd::Getopt::argument_required,   NULL,   'i'  },
+	{ "file",	inetd::Getopt::argument_required,   NULL,   'f'  },
+	{ "quiet",	inetd::Getopt::argument_none,	    NULL,   'q'  },
+	{ "verbose",	inetd::Getopt::argument_none,	    NULL,   'v'  },
+	{ "usage",	inetd::Getopt::argument_none,	    NULL,   1100 },
+	{ NULL }
+	};
 
 namespace {
 // left trim
@@ -102,74 +102,87 @@ bool parse_file(const inetd::Getopt &options, const char *filename, Pred &pred)
 int
 main(int argc, const char **argv)
 {
-    inetd::Getopt options(short_options, long_options);
-    std::vector<const char *> ips, files;
-    std::string errmsg;
-    const char *database = nullptr;
-    bool verbose = false;
+	inetd::Getopt options(short_options, long_options);
+	std::vector<const char *> ips, files;
+	std::string errmsg;
+	const char *database = nullptr;
+	bool verbose = false, quiet = false;
 
-    while (-1 != options.shift(argc, argv, errmsg)) {
-        switch (options.optret()) {
-        case 'd': {
-                const char *t_database = options.optarg();
-                if (!*t_database) {
-                    usage(options, "empty database");
-                } else if (database) {
-                    usage(options, "multiple database's specified <%s> and <%s>", database, t_database);
-                }
-                database = t_database;
-            }
-            break;
-        case 'f':
-            files.push_back(options.optarg());
-            break;
-        case 'i':
-            ips.push_back(options.optarg());
-            break;
-        case 'v':
-            verbose = true;
-            break;
-        case 1100:          // usage
-        case '?':
-            usage(options, nullptr);
-        default:            // error
-            usage(options, "%s", errmsg.c_str());
-        }
-    }
+	while (-1 != options.shift(argc, argv, errmsg)) {
+		switch (options.optret()) {
+		case 'd': {
+				const char *t_database = options.optarg();
+				if (!*t_database) {
+					usage(options, "empty database");
+				} else if (database) {
+					usage(options, "multiple database's specified <%s> and <%s>", database, t_database);
+				}
+				database = t_database;
+			}
+			break;
+		case 'f':
+			files.push_back(options.optarg());
+			break;
+		case 'i':
+			ips.push_back(options.optarg());
+			break;
+		case 'v':
+			verbose = true;
+			break;
+		case 'q':
+			quiet = true;
+			break;
+		case 1100:	// usage
+		case '?':
+			usage(options, nullptr);
+		default:	// error
+			usage(options, "%s", errmsg.c_str());
+		}
+	}
 
-    if (nullptr == database) {
-        usage(options, "database missing");
-    }
+	if (nullptr == database) {
+		usage(options, "database missing");
+	}
 
-    argv += options.optind();
-    if (0 != (argc -= options.optind())) {
-        usage(options, "unexpected arguments %s ...", argv[0]);
-    }
+	argv += options.optind();
+	if (0 != (argc -= options.optind())) {
+		usage(options, "unexpected arguments %s ...", argv[0]);
+	}
 
 #if defined(HAVE_LIBMAXMINDDB)
-    MMDB_s mmdb;
-    if (! mmdbopen(database, &mmdb))
-        return 3;
-    if (verbose)
-        mmdbmeta(&mmdb);
-    for (auto ip : ips)
-        mmdblookup(&mmdb, ip);    
-    for (auto file : files) {
-        parse_file<>(options, file, [&](std::string &line) {
-		        const size_t comma = line.find_first_of(',');
-                        if (comma != std::string::npos) { // first element
-			        std::string ip(line, 0, comma);
-                                mmdblookup(&mmdb, ip.c_str());   
-                        } else { // whole element
-                                mmdblookup(&mmdb, line.c_str());                      
-                        }
-                        return true;
-                        });
-    }
-    MMDB_close(&mmdb);
+	MMDB_s mmdb;
+	if (! mmdbopen(database, &mmdb))
+		return 3;
+	if (verbose)
+		mmdbmeta(&mmdb);
+	for (auto ip : ips)
+		mmdblookup(&mmdb, ip, false);
+	for (auto file : files) {
+		clock_t const clock_start = clock();
+		unsigned count = 0;
+		parse_file<>(options, file, [&](std::string &line) {
+				const size_t comma = line.find_first_of(',');
+				if (comma != std::string::npos) { // first element
+					std::string ip(line, 0, comma);
+					mmdblookup(&mmdb, ip.c_str(), quiet);
+				} else { // whole element
+					mmdblookup(&mmdb, line.c_str(), quiet);
+				}
+				++count;
+				return true;
+				});
+
+		if (verbose) {
+			clock_t const clock_diff = clock() - clock_start;
+			double const seconds = (double)clock_diff / CLOCKS_PER_SEC;
+			fprintf(stdout, "%u addresses in %.2f seconds. %.2Lf lookups per second.\n",
+					count, seconds, (long double)count / seconds);
+		}
+	}
+	MMDB_close(&mmdb);
 
 #else
-    usage("libmaxminddb support not enabled");
+	usage("libmaxminddb support not enabled");
 #endif
     return 0;
 }
@@ -178,25 +191,26 @@ main(int argc, const char **argv)
 static void
 usage(const inetd::Getopt &options, const char *fmt, ...)
 {
-    if (fmt) {
-        va_list ap;
-        va_start(ap, fmt);
-        vfprintf(stderr, fmt, ap), fputs("\n\n", stderr);
-        va_end(ap);
-    }
+	if (fmt) {
+		va_list ap;
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap), fputs("\n\n", stderr);
+		va_end(ap);
+	}
 
-    fprintf(stderr,
-        "Usage: %s [options] --db <mmdb> --file <file> | --ip <addr> ..\n\n", options.progname());
-    fprintf(stderr,
-        "options:\n"
-        "   -v,--verbose                Verbose.\n"
-        "\n"
-        "arguments:\n"
-        "   -d,--db <database>          MMDB file path, required.\n"
-        "   -i,--ip <path>              Address to resolve, none or more.\n"
-        "   -f,--file <file>            Address list, none or more.\n"
-        );
-    exit(3);
+	fprintf(stderr,
+	    "Usage: %s [options] --db <mmdb> --file <file> | --ip <addr> ..\n\n", options.progname());
+	fprintf(stderr,
+	    "options:\n"
+	    " -v,--verbose         Database meta and performance stats.\n"
+	    " -q,--quiet           Quiet file mode; lookup only, wont dump associated data-set.\n"
+	    "\n"
+	    "arguments:\n"
+	    " -d,--db <database>   MMDB file path, required.\n"
+	    " -i,--ip <path>       Address to resolve, none or more.\n"
+	    " -f,--file <file>     Address list, none or more.\n"
+	    );
+	exit(3);
 }
 
 
@@ -204,106 +218,108 @@ usage(const inetd::Getopt &options, const char *fmt, ...)
 static bool
 mmdbopen(const char *filename, MMDB_s *mmdb)
 {
-    int status = MMDB_open(filename, MMDB_MODE_MMAP, mmdb);
-    if (MMDB_SUCCESS != status) {
-        fprintf(stderr, "\n  Can't open %s - %s\n", filename, MMDB_strerror(status));
-        if (MMDB_IO_ERROR == status) {
-            fprintf(stderr, "    IO error: %s\n", strerror(errno));
-        }
-        return false;
-    }
-    return true;
+	int status = MMDB_open(filename, MMDB_MODE_MMAP, mmdb);
+	if (MMDB_SUCCESS != status) {
+		fprintf(stderr, "\n  Can't open %s - %s\n", filename, MMDB_strerror(status));
+		if (MMDB_IO_ERROR == status) {
+			fprintf(stderr, "    IO error: %s\n", strerror(errno));
+		}
+		return false;
+	}
+	return true;
 }
 
 
 static void 
 mmdbmeta(MMDB_s *mmdb)
 {
-    const char *meta_dump = "\n"
-                            "  Database metadata\n"
-                            "    Node count:    %i\n"
-                            "    Record size:   %i bits\n"
-                            "    IP version:    IPv%i\n"
-                            "    Binary format: %i.%i\n"
-                            "    Build epoch:   %llu (%s)\n"
-                            "    Type:          %s\n"
-                            "    Languages:     ";
+	const char *meta_dump =
+	        "\n"
+	        "  Metadata:\n"
+	        "    Node count:    %i\n"
+	        "    Record size:   %i bits\n"
+	        "    IP version:    IPv%i\n"
+	        "    Binary format: %i.%i\n"
+	        "    Build epoch:   %llu (%s)\n"
+	        "    Type:          %s\n"
+	        "  Languages:       ";
+	const char *description =
+	        "  Description:\n";
 
-    char date[40];
-    const time_t epoch = (const time_t)mmdb->metadata.build_epoch;
-    strftime(date, 40, "%F %T UTC", gmtime(&epoch));
+	char date[40];
+	const time_t epoch = (const time_t)mmdb->metadata.build_epoch;
+	strftime(date, 40, "%F %T UTC", gmtime(&epoch));
 
-    fprintf(stdout,
-            meta_dump,
-            mmdb->metadata.node_count,
-            mmdb->metadata.record_size,
-            mmdb->metadata.ip_version,
-            mmdb->metadata.binary_format_major_version,
-            mmdb->metadata.binary_format_minor_version,
-            mmdb->metadata.build_epoch,
-            date,
-            mmdb->metadata.database_type);
+	fprintf(stdout, meta_dump,
+		mmdb->metadata.node_count,
+		mmdb->metadata.record_size,
+		mmdb->metadata.ip_version,
+		mmdb->metadata.binary_format_major_version,
+		mmdb->metadata.binary_format_minor_version,
+		mmdb->metadata.build_epoch,
+		date,
+		mmdb->metadata.database_type);
 
-    for (size_t i = 0; i < mmdb->metadata.languages.count; i++) {
-        fprintf(stdout, "%s", mmdb->metadata.languages.names[i]);
-        if (i < mmdb->metadata.languages.count - 1) {
-            fprintf(stdout, " ");
-        }
-    }
-    fprintf(stdout, "\n");
+	for (size_t i = 0; i < mmdb->metadata.languages.count; i++) {
+		fprintf(stdout, "%s", mmdb->metadata.languages.names[i]);
+		if (i < mmdb->metadata.languages.count - 1) {
+			fprintf(stdout, " ");
+		}
+	}
+	fprintf(stdout, "\n");
 
-    fprintf(stdout, "    Description:\n");
-    for (size_t i = 0; i < mmdb->metadata.description.count; i++) {
-        fprintf(stdout,
-                "      %s:   %s\n",
-                mmdb->metadata.description.descriptions[i]->language,
-                mmdb->metadata.description.descriptions[i]->description);
-    }
-    fprintf(stdout, "\n");
+	fprintf(stdout, description);
+	for (size_t i = 0; i < mmdb->metadata.description.count; i++) {
+		fprintf(stdout, "    %s: %s\n",
+			mmdb->metadata.description.descriptions[i]->language,
+			mmdb->metadata.description.descriptions[i]->description);
+	}
+	fprintf(stdout, "\n");
+	fflush(stdout);
 }
 
 
 static int
-mmdblookup(MMDB_s *mmdb, const char *ip_address)
+mmdblookup(MMDB_s *mmdb, const char *ip_address, bool quiet)
 {
-    int gai_error, mmdb_error;
-    MMDB_lookup_result_s result =
-        MMDB_lookup_string(mmdb, ip_address, &gai_error, &mmdb_error);
-    if (0 != gai_error) {
-        fprintf(stderr, "\n  Error from getaddrinfo for %s - %s\n\n", ip_address, gai_strerror(gai_error));
-        return -2;
-    }
+	int gai_error, mmdb_error;
+	MMDB_lookup_result_s result =
+		MMDB_lookup_string(mmdb, ip_address, &gai_error, &mmdb_error);
+	if (0 != gai_error) {
+		fprintf(stderr, "\n  Error from getaddrinfo for %s - %s\n\n", ip_address, gai_strerror(gai_error));
+		return -2;
+	}
 
-    if (MMDB_SUCCESS != mmdb_error) {
-        fprintf(stderr, "\n  Got an error from libmaxminddb: %s\n\n", MMDB_strerror(mmdb_error));
-        return -2;
-    }
+	if (MMDB_SUCCESS != mmdb_error) {
+		fprintf(stderr, "\n  Got an error from libmaxminddb: %s\n\n", MMDB_strerror(mmdb_error));
+		return -2;
+	}
 
-    MMDB_entry_data_list_s *entry_data_list = NULL;
+	printf("IP: %s\n", ip_address);
+	MMDB_entry_data_list_s *entry_data_list = NULL;
+	int rtn = 0;
 
-    int exit_code = 0;
-    printf("IP: %s\n", ip_address);
-    if (result.found_entry) {
-        const int status = MMDB_get_entry_data_list(&result.entry, &entry_data_list);
-        if (MMDB_SUCCESS != status) {
-            fprintf(stderr, "Got an error looking up the entry data - %s\n", MMDB_strerror(status));
-            exit_code = 4;
-            goto end;
-        }
+	if (result.found_entry) {
+		const int status = MMDB_get_entry_data_list(&result.entry, &entry_data_list);
+		if (MMDB_SUCCESS != status) {
+			fprintf(stderr, "Got an error looking up the entry data - %s\n", MMDB_strerror(status));
+			rtn = 4;
+			goto end;
+		}
 
-        if (NULL != entry_data_list) {
-            MMDB_dump_entry_data_list(stdout, entry_data_list, 2);
-        }
+		if (NULL != entry_data_list && !quiet) {
+			MMDB_dump_entry_data_list(stdout, entry_data_list, 2);
+		}
 
-    } else {
-        fprintf(stderr, "\n  No entry for this IP address (%s) was found\n\n", ip_address);
-        exit_code = 5;
-    }
+	} else {
+		fprintf(stderr, "\n  No entry for this IP address (%s) was found\n\n", ip_address);
+		rtn = 5;
+	}
 
 end:
-    printf("\n");
-    MMDB_free_entry_data_list(entry_data_list);
-    return exit_code;
+	if (!quiet) printf("\n");
+	MMDB_free_entry_data_list(entry_data_list);
+	return rtn;
 }
 #endif //HAVE_LIBMAXMINDDB
 
